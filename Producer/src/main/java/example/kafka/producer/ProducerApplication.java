97469557cdf7777284d096f89eeb39798f652c7e
Ankur Papneja <ankurpapneja4@gmail.com>
2024-02-29 12:07:23 +0530

    KafkaProducerService Added

--- Producer/src/main/java/example/kafka/producer/ProducerApplication.java
+++ Producer/src/main/java/example/kafka/producer/ProducerApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class ProducerApplication {
 
 	public static void main(String[] args) {
@@ -20,4 +24,5 @@ public class ProducerApplication {
 					.replicas(3)
 					.build();
 	}
+
 }
