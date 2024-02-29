324f2f5af2d8a4e5478b70cd149c97832290ab1b
Ankur Papneja <ankurpapneja4@gmail.com>
2024-02-29 11:25:32 +0530

    Create New Topic In Kafka Cluster

--- Producer/src/main/java/example/kafka/producer/ProducerApplication.java
+++ Producer/src/main/java/example/kafka/producer/ProducerApplication.java
@@ -1,7 +1,10 @@
 package example.kafka.producer;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class ProducerApplication {
@@ -10,4 +13,11 @@ public class ProducerApplication {
 		SpringApplication.run(ProducerApplication.class, args);
 	}
 
+	@Bean
+	public NewTopic topic(){
+		return TopicBuilder.name("topic1")
+					.partitions(3)
+					.replicas(3)
+					.build();
+	}
 }
